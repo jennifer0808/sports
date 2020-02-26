224d863df2df7e76360d314b0b356ad4d79fb722
jennifer <wj_computer@outlook.com>
2020-02-26 17:30:58 +0800

    1.mybatis-spring不需要配置一个完整的mybatis配置；logImpl-log4j2解决log4j:WARN 2.配置log4j2日志

--- src/test/java/com/jennifer/sportsmeeting/SportsmeetingApplicationTests.java
+++ src/test/java/com/jennifer/sportsmeeting/SportsmeetingApplicationTests.java
@@ -1,13 +1,25 @@
 package com.jennifer.sportsmeeting;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 class SportsmeetingApplicationTests {
 
+    @Resource
+    private DataSource dataSource;
     @Test
     void contextLoads() {
+        System.out.println(this.dataSource);
     }
 
 }
